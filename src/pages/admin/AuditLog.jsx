f5780a8be7d2324f542f2df63e0de1e7acfd3fb4
sabeloreabetswe-hue <sabeloreabetswe@gmail.com>
2026-07-29 13:11:12 +0200

    Create AuditLog.jsx

---
+++ src/pages/admin/AuditLog.jsx
@@ -0,0 +1,24 @@
+import Sidebar from "../components/dashboard/Sidebar";
+import Topbar from "../components/dashboard/Topbar";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen bg-[#FAF6E3]">
+
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+
+        <Topbar />
+
+        <main className="p-8">
+          {children}
+        </main>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default DashboardLayout;
